7786d5f9f445dc6ec2ef2438ad5b0001ddfa568e
ManoloGioino <jmgioino@mail.austral.edu.ar>
2024-09-05 12:01:43 -0300

    entrega

--- src/main/java/com/template/Elevator.java
+++ src/main/java/com/template/Elevator.java
@@ -1,11 +1,50 @@
 package com.template;
 
 public class Elevator {
+    private int currentFloor;
+    private final int maxFloor;
+    private int passengerCount;
+    private int maxPassengers;
 
+    // Constructor
+    public Elevator(int currentFloor, int maxFloor, int maxPassengers) {
+        this.currentFloor = currentFloor;
+        this.maxFloor = maxFloor;
+        this.maxPassengers = maxPassengers;
+        this.passengerCount = 0;
+    }
 
+    // Move to the requested floor
+    public void moveToFloor(int requestedFloor) {
+        int minFloor = 0;
+        if (requestedFloor >= minFloor && requestedFloor <= maxFloor) {
+            this.currentFloor = requestedFloor;
+        }
+    }
 
-    // Implement this class by observing the tests in this repo. From those you will be able
-    // to pick what the constructors params are, what the methods are and what they should do.
+    // Add a passenger to the elevator
+    public void addPassenger() {
+        if (passengerCount < maxPassengers) {
+            passengerCount++;
+        }
+    }
 
+    // Remove a passenger from the elevator
+    public void removePassenger() {
+        if (passengerCount > 0) {
+            passengerCount--;
+        }
+    }
 
+    // Get the current floor of the elevator
+    public int getCurrentFloor() {
+        return currentFloor;
+    }
+
+    // Get the current number of passengers in the elevator
+    public int getPassengerCount() {
+        return passengerCount;
+    }
 }
+
+
